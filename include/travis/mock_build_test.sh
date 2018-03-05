a0b3063a4018a36b487c231d6f05bbf3a748c7db
Joe Grund <joe.grund@intel.com>
2018-03-05 11:25:51 -0500

    another fixup
    Signed-off-by: Joe Grund <joe.grund@intel.com>

--- include/travis/mock_build_test.sh
+++ include/travis/mock_build_test.sh
@@ -4,7 +4,7 @@
 MAPPED_DIR="${MAPPED_DIR:-/build}"
 
 echo 'travis_fold:start:yum'
-docker exec -i "$DOCKER_CONTAINER_ID" yum -y install git mock rpm-build ed sudo make rpmdevtools python-setuptools rpmlint
+yum -y install git mock rpm-build ed sudo make rpmdevtools python-setuptools
 echo 'travis_fold:end:yum'
 
 # add our repos to the mock configuration
